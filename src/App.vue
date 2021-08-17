49a92b722fe1dcca0b2d414d52f49ff3269fa626
egovframedev <whoisy@daum.net>
2021-08-17 17:20:02 +0900

    first vue3 template

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ message }}</h1>
+  <img
+    src="~assets/logo.png"
+    alt="Vue logo" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!!!'
+    }
+  }
+}
+</script>
